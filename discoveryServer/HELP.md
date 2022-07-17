ad1d45a7fb12d686059bcb6aca379a3111833469
JiHyeon Yu <70328749+jihyeon121223@users.noreply.github.com>
2022-07-17 19:30:37 +0900

    스프링이 처음이면 HELP.md를 참고

--- discoveryServer/HELP.md
+++ discoveryServer/HELP.md
@@ -1,3 +1,12 @@
+# 지혀니 설명
+이 프로젝트를 켜면 이걸 읽어보시오.
+다른건 워크프레임으로 자동 구성되니 해당 파일만 손대면 되오. 알아서 참조하여 구동될거요.
+우선 제일 밖(밑)에 있는 pom.xml을 보시오. 
+두번째로 src>main>resources>pplication.yml을 보시오. 설명을 대따 적어놨소. 
+세번째로 src>main>java>com.example>DiscoveryServerApplication.java을 보시오. 어노테이션 파일은 각각 따로 만드시오. 설명은 이게 끝이오. 
+
+
+
 # Getting Started
 
 ### Reference Documentation
--- discoveryServer/src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
+++ discoveryServer/src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
@@ -1,8 +1,11 @@
+//마지막으로 main클래스 위에 @EnableEurekaServer 어노테이션을 지정
+
 package com.example.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
 @EnableEurekaServer
